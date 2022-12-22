aa810a13e653cfc64ea80e4f229a5489e7102eb1
Shivamsanskari <Shivamm733@gmail.com>
2022-12-22 10:52:20 +0530

    Added the input and Dynamic counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,20 @@
 import classes from './Counter.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { useState } from 'react';
 
 const Counter = () => {
 
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const showHide = useSelector((state) => state.counterStatus);
+
+
+  const [userInput, setUserInput] = useState('');
+
+  const userInputHandler = (e) => {
+    e.preventDefault();
+    setUserInput(e.target.value);
+  }
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
@@ -14,14 +23,22 @@ const Counter = () => {
     dispatch({ type: 'decrement' });
   }
 
-  const toggleCounterHandler = () => { };
+  const increaseHandler = () => {
+    dispatch({type: 'increase', payload: 20})
+  }
+
+  const toggleCounterHandler = () => {
+      dispatch({type: 'togglecounter'})
+   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showHide && <div className={classes.value}>{counter}</div>}
+      <input type="text" value={userInput} onChange={userInputHandler}/>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase By 20</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,35 @@
 import { legacy_createStore as createStore} from 'redux';
 
-const counterReducer = (state = {counter: 0}, action) => {
+
+const initialState = {counter: 0, counterStatus: true}
+
+const counterReducer = (state = initialState, action) => {
     if(action.type === 'increment') {
         return {
             counter: state.counter + 1,
+            counterStatus: state.counterStatus
         };
     }
     if(action.type === 'decrement') {
         return {
             counter: state.counter - 1,
+            counterStatus: state.counterStatus
         };
     }
+    if(action.type === 'increase') {
+        return {
+            counter: state.counter + action.payload,
+            counterStatus: state.counterStatus
+        };
+    }
+
+    if(action.type === 'togglecounter'){
+        return {
+            counter: state.counter,
+            counterStatus: !state.counterStatus
+        }
+    }
+
     return state;
 }
 const store = createStore(counterReducer);
